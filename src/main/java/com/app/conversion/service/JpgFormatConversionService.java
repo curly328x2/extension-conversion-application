066187a8c0f82fceff5061b312577248b0bdb89d
curly328x2 <curly328x2@gmail.com>
2024-10-27 17:00:24 +0900

    フォルダからファイル読み込み

--- src/main/java/com/app/conversion/service/JpgFormatConversionService.java
+++ src/main/java/com/app/conversion/service/JpgFormatConversionService.java
@@ -1,5 +1,6 @@
 package com.app.conversion.service;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public interface JpgFormatConversionService {
@@ -7,5 +8,5 @@ public interface JpgFormatConversionService {
     /**
      * jpgから任意の拡張子へ変換する処理の実行
      */
-    public Path jpgFormatConversion(String inputFilePath, String outputDirectory);
+    public Path jpgFormatConversion(String inputFilePath, String outputDirectory) throws IOException;
 }
--- src/main/java/com/app/conversion/service/JpgFormatConversionServiceImpl.java
+++ src/main/java/com/app/conversion/service/JpgFormatConversionServiceImpl.java
@@ -1,14 +1,36 @@
 package com.app.conversion.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@SuppressWarnings("resource")
 public class JpgFormatConversionServiceImpl implements JpgFormatConversionService {
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Path jpgFormatConversion(String inputFilePath, String outputDirectory){
+    public Path jpgFormatConversion(String inputFilePath, String outputDirectory) throws IOException {
+        // フォルダ内のファイルを読み込む
+        List<String> readFiles = readAllFiles(inputFilePath);
         return null;
     }
+
+    /**
+     * フォルダ内のすべてのファイルを読み込む
+     *
+     * @return 読み込まれたすべてのファイル
+     */
+    private List<String> readAllFiles(String inputFilePath) throws IOException {
+        return Files.walk(Paths.get(inputFilePath))
+                .filter(Files::isRegularFile)
+                .map(Path::toString)
+                .filter(string -> string.endsWith(".jpg"))
+                .collect(Collectors.toList());
+    }
+
 }
